7cae0312d57b4a35305a2e9a4ff0d5489c1a6700
Emmanuel <emmanue.kimweli@student.moringaschool.com>
2022-09-09 21:11:31 +0300

    add manu.json file

---
+++ manu.js
@@ -0,0 +1,14 @@
+// function makeABooking(booking){
+//     let booked = document.createElement('div');
+    
+//     booked.innerHTML = `
+//     <form  id="booked" >
+//     <p>NAME: ${bookings.name}</p>
+//     <p>NO OF ROOMS: ${bookings.NoOfRooms}</p>
+
+// </form>
+    
+//     `
+//     document.queryselector('#form').appendChild(booked);
+
+// }
