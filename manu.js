9e54a417d2559ff1aa6a15993708dbb1b3ae7db8
Emmanuel <emmanue.kimweli@student.moringaschool.com>
2022-09-10 18:18:17 +0300

    add eventListener on the submit button

--- manu.js
+++ manu.js
@@ -1,14 +1,62 @@
-// function makeABooking(booking){
-//     let booked = document.createElement('div');
-    
-//     booked.innerHTML = `
-//     <form  id="booked" >
-//     <p>NAME: ${bookings.name}</p>
-//     <p>NO OF ROOMS: ${bookings.NoOfRooms}</p>
+document.getElementById("form-book").addEventListener("submit",submitButton);
 
-// </form>
-    
-//     `
-//     document.queryselector('#form').appendChild(booked);
 
-// }
+function submitButton(e){
+    e.preventDefault();
+
+    let bookObj = {
+        name:e.target.name.value,
+        arrivalDate:e.target.arrivalDate.value,
+        departureDate:e.target.departureDate.value,
+        noOfAdults:e.target.noOfAdults.value,
+        noOfChildren:e.target.noOfChildren.value,
+        noOfRooms:e.target.noOfRooms.value,
+        formOfPayment:e.target.formOfPayment.value,
+    }
+
+    madeABooking(bookObj);
+    makeABooking(bookObj);
+}
+
+//fetch
+function getTheBookings(){
+    fetch("http://localhost:3000")
+    .then(res => res.json())
+    .then(bookings => bookings.forEach(booking => madeABooking(booking)));
+}
+
+function makeABooking(bookObj){
+    // console.log(bookObj);
+  fetch("http://localhost:3000",{
+    method: "POST",
+    headers:{
+        'content-Type':'application/json'
+    },
+     body:JSON.stringify(bookObj)
+  })
+  .then(res => res.json())
+  .then(bookings => bookings.forEach(booking => makeABooking(booking)));
+}
+
+// makeABooking();
+
+//DOM manipulation
+
+function madeABooking(booking){
+    let booked = document.createElement('div');
+    
+    booked.innerHTML = `
+    <form  id="booked" >
+    <p>NAME: ${booking.name}</p>
+    <p>Arrival date: ${booking.arrival}</p>
+    <p>Departure date: ${booking.departureDates}</p>
+    <p>NO OF ROOMS: ${booking.NoOfRooms}</p>
+    <button typy="submit" id='delete'>delete</button>
+    
+
+</form>
+    
+    `
+    document.getElementById('form-book').appendChild(booked);
+
+}
